b8a194db0120af16a7407e4ea0395d2cbbc419b6
Matheus César <146596138+Matheus0197M@users.noreply.github.com>
2025-06-07 12:38:02 +0000

    feat::ajustes finais

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  let lastScroll = window.scrollY;
+let lastScroll = window.scrollY;
   const header = document.getElementById('header');
 
   window.addEventListener('scroll', () => {
@@ -6,7 +6,7 @@
 
     if (currentScroll > lastScroll) {
       // Rolar pra baixo
-      header.style.top = "-150px"; // ajuste se o header for mais alto
+      header.style.top = "-170px"; // ajuste se o header for mais alto
     } else {
       // Rolar pra cima
       header.style.top = "0";
@@ -18,6 +18,6 @@
 
   setTimeout(() => {
     const header = document.getElementById('header');
-    header.style.top = "-150px"; // Ajuste conforme a altura do seu header
+    header.style.top = "-170px"; // Ajuste conforme a altura do seu header
   }, 3000); // 3000 milissegundos = 3 segundos
 
